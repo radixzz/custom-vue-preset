8a5f12e0261beeddc130534a95b0a02b0dc6a5e9
Iván Juárez <ivan@blindgazer.com>
2019-04-11 10:35:22 -0500

    feat: copying assets folder with webpack plugin

--- generator/config/vue.config.template.js
+++ generator/config/vue.config.template.js
@@ -1,3 +1,4 @@
+const CopyPlugin = require('copy-webpack-plugin');
 const rucksack = require('rucksack-css');
 const rupture = require('rupture');
 const nib = require('nib');
@@ -5,56 +6,66 @@ const lost = require('lost');
 const path = require('path');
 
 function resolve(dir) {
-  return path.join(__dirname, dir);
+    return path.join(__dirname, dir);
 }
 
 module.exports = {
-  css: {
-    loaderOptions: {
-      stylus: {
-        use: [
-          nib(),
-          rupture(),
-        ],
-        import: [
-          '~nib/lib/nib/index.styl',
-          '~rupture/rupture/index.styl',
-        ],
-      },
-      postcss: {
+    publicPath: '/',
+    // publicPath: '/foo/clients',
+    // outputDir: resolve('../public/front'),
+    assetsDir: 'assets',
+    configureWebpack: {
         plugins: [
-          rucksack({
-            autoprefixer: false,
-            fallbacks: false
-          }),
-          lost(),
-        ],
-      }
+            new CopyPlugin([
+                { from: 'static', to: 'assets' },
+            ]),
+        ]
+    },
+    css: {
+        loaderOptions: {
+            stylus: {
+                use: [
+                    nib(),
+                    rupture(),
+                ],
+                import: [
+                    '~nib/lib/nib/index.styl',
+                    '~rupture/rupture/index.styl',
+                ],
+            },
+            postcss: {
+                plugins: [
+                    rucksack({
+                        autoprefixer: false,
+                        fallbacks: false
+                    }),
+                    lost(),
+                ],
+            }
+        }
+    },
+    chainWebpack: (config) => {
+        const { alias } = config.resolve;
+        alias.set('@', resolve('src'));
+        alias.set('src', resolve('src'));
+        alias.set('foo', resolve('src/foo'));
+        alias.set('assets', resolve('src/assets'));
+        alias.set('styles', resolve('src/styles'));
+        /*
+        JSON rule to work with file-loader again, as of webpack 4.0
+        all JSON's are handled natively see: https://github.com/webpack/webpack/releases/tag/v4.0.0
+        and because of this if you try to do a: require or an import of the file
+        you'll get the JSON's contents(wrapped with an export call) instead of the generated URL.
+        */
+        config.module
+            .rule('json')
+            .test(/\.(json)(\?.*)?$/)
+            .type('javascript/auto')
+            .use('file-loader')
+            .loader('file-loader')
+            .tap((options = {}) => {
+                options.name = 'assets/data/[name].[hash:8].[ext]';
+                return options;
+            })
     }
-  },
-  chainWebpack: (config) => {
-    const { alias } = config.resolve;
-    alias.set('@', resolve('src'));
-    alias.set('src', resolve('src'));
-    alias.set('foo', resolve('src/foo'));
-    alias.set('assets', resolve('src/assets'));
-    alias.set('styles', resolve('src/styles'));
-
-    /*
-      JSON rule to work with file-loader again, as of webpack 4.0
-      all JSON's are handled natively see: https://github.com/webpack/webpack/releases/tag/v4.0.0
-      and because of this if you try to do a: require or an import of the file
-      you'll get the JSON's contents(wrapped with an export call) instead of the generated URL.
-    */
-    config.module
-      .rule('json')
-      .test(/\.(json)(\?.*)?$/)
-      .type('javascript/auto')
-      .use('file-loader')
-      .loader('file-loader')
-      .tap((options = {}) => {
-        options.name = 'data/[name].[hash:8].[ext]';
-        return options;
-      })
-  }
 };
